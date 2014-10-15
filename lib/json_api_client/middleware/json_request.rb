7961a9057d2456592bcce306907e0cf6955c1766
Justin Smestad <justin.smestad@gmail.com>
2014-10-15 11:52:31 -0600

    remove .json append in middleware; only accept json

--- lib/json_api_client/middleware/json_request.rb
+++ lib/json_api_client/middleware/json_request.rb
@@ -2,9 +2,10 @@ module JsonApiClient
   module Middleware
     class JsonRequest < Faraday::Middleware
       def call(environment)
-        environment[:request_headers]["Accept"] = "application/json,*/*"
+        environment[:request_headers]["Accept"] = "application/json"
         uri = environment[:url]
-        uri.path = uri.path + ".json" unless uri.path.match(/\.json$/) 
+        # FIXME
+        # uri.path = uri.path + ".json" unless uri.path.match(/\.json$/)
         @app.call(environment)
       end
     end
